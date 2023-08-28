7a9f461941bbbab2ab915c7d57d47fdc8ec942f8
avasl <alexvanaxe@gmail.com>
2023-08-28 15:19:05 -0300

    Initial Commit

---
+++ api/flake.nix
@@ -0,0 +1,15 @@
+{
+    description = "This is the bible project."
+
+    inputs = {
+    };
+
+    outputs = { self, nixpkgs, ...}@inputs:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+        devShells.x86_64-linux.default = import ./shell.nix { inherit pkgs; };
+
+    };
+}
