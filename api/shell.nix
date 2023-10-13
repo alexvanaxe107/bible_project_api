38ad38e68046756824ea9056e4eb9697ebff641b
avasl <alexvanaxe@gmail.com>
2023-10-13 16:15:18 -0300

    Added postgresql to the shell

--- api/shell.nix
+++ api/shell.nix
@@ -1,8 +1,37 @@
 { pkgs ? import <nixpkgs> {} }:
+  # ENV Variables
+
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [ python3 sqlite ];
+    nativeBuildInputs = with pkgs.buildPackages; [ python3 sqlite postgresql ];
+
+
+    # LD_LIBRARY_PATH = "${geos}/lib:${gdal}/lib";
+
     shellHook = ''
     source .venv/bin/activate
+    export PGDATA="$PWD/.pg"
+
+    _start_pg(){
+        pg_ctl -w -l $PGDATA/log -o "--unix_socket_directories=$PWD" start &> /dev/null
+    }
+    _stop_pg(){
+        pg_ctl stop &> /dev/null
+    }
+
+
+    if [ ! -d $PGDATA ]; then
+        initdb &> /dev/null
+        CREATE=true
+    fi
+
+    _start_pg
+    # register_cleanup _stop_pg # ~ trapping HUP/EXIT
+
+    if [[ $CREATE ]]; then
+        createuser -s postgres &> /dev/null
+        # set a stable crappy password for local/dev
+        # create db
+    fi
     '';
 }
