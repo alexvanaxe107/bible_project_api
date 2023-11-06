f80a02ba56159d9a9668777f83b80989aed54d88
avasl <alexvanaxe@gmail.com>
2023-11-06 15:35:17 -0300

    Removendo web

--- api/app/sql/crud.py
+++ api/app/sql/crud.py
@@ -6,8 +6,10 @@ from . import models, schemas
 def get_book(db: Session, book: str):
     return db.query(models.Book).filter(models.Book.id == book).first()
 
+
 def get_chapter(db: Session, book: str, chapter: str):
-    return db.query(models.Verse).filter((models.Verse.book_id == book) & (models.Verse.chapter == chapter)).all()
+    return db.query(models.Verse).filter((models.Verse.book_id == book) & (models.Verse.chapter == chapter)).order_by(models.Verse.id).all()
+
 
 def get_verse(db: Session, verse: str):
     return db.query(models.Verse).filter(models.Verse.id == verse).first()
